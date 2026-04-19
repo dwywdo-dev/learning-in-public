a33bdc2704b8aa290666e4421d4ac1d000dfc752
dwywdo-dev <dwywdo.dev@gmail.com>
2026-04-19 21:48:32 +0900

    Initialize Spring Boot Up and Running - CH06

---
+++ spring-boot-up-and-running-ch6/src/main/java/me/dwywdo/springbootupandrunningch6/SpringBootUpAndRunningCh6Application.java
@@ -0,0 +1,13 @@
+package me.dwywdo.springbootupandrunningch6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUpAndRunningCh6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUpAndRunningCh6Application.class, args);
+    }
+
+}
---
+++ spring-boot-up-and-running-ch6/src/test/java/me/dwywdo/springbootupandrunningch6/SpringBootUpAndRunningCh6ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dwywdo.springbootupandrunningch6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUpAndRunningCh6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
